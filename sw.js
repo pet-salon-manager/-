5e0ff9bcba60f8a36fcc87be1ac9aba0d95d68b1
pet-salon-manager <sinntyoukoukichild@yahoo.co.jp>
2026-08-12 04:35:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE='pawpal-v18-3-places-20260812';
+const CACHE='pawpal-v18-4-osm-places-20260812';
 const ASSETS=['./','index.html','style.css','app.js','manifest.json'];
 
 self.addEventListener('install',e=>{
